98c9fbde33715d6a26c75e1ca7cec9a890f334a9
Gustav Eikaas <46537983+GustavEikaas@users.noreply.github.com>
2023-08-23 20:04:29 +0200

    :sparkles: package.json parser :bookmark: release 0.1.0

---
+++ Changelog.md
@@ -0,0 +1,9 @@
+## Changelog
+
+## 0.1.0
+
+✨ add Package.json parser using Zod
+
+## 0.0.1
+
+✨ Add ANSI logger
---
+++ src/packageJsonParser/index.ts
@@ -0,0 +1,42 @@
+import { readFileSync } from "fs";
+import { resolve } from "path";
+
+import { z } from "zod";
+
+const PackageJsonSchema = z.object({
+  name: z.string(),
+  version: z.string(),
+  description: z.string().optional(),
+});
+
+/**
+ * Function for parsing a package.json
+ */
+export function parsePackageJson(
+  path = "./package.json"
+): (typeof PackageJsonSchema)["_type"] {
+  try {
+    const packageJson = readPackageJson(path);
+    const validatedPackageJson = validatePackageJsonContents(packageJson);
+    return validatedPackageJson;
+  } catch (e) {
+    console.log("Failed to find package.json", e);
+    throw new Error("");
+  }
+}
+
+function readPackageJson(path: string) {
+  const location = resolve(path);
+  const contents = readFileSync(location, { encoding: "utf8" });
+
+  const packageJson = JSON.parse(contents);
+  return packageJson;
+}
+
+function validatePackageJsonContents(a: Record<PropertyKey, unknown>) {
+  try {
+    return PackageJsonSchema.parse(a);
+  } catch (e) {
+    throw new Error("Failed to parse package.json", { cause: e });
+  }
+}
